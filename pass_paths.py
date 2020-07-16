ce0af48a400291ac5aa88a1ff6ee6f0f511a6bd9
Rob Wells <vaandei421@gmail.com>
2020-07-15 22:46:55 -0600

    points added

--- pass_paths.py
+++ pass_paths.py
@@ -1,80 +1,136 @@
+#=========================================================================================================================#
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation
 import json
 
 import tools as tl
+import plots as pl
 
 # this file will make a list of the path routes
 # will make paths before possession switches
+#=========================================================================================================================#
 
-with open('3749052.json') as f:
-    data = json.load(f)
+def main():
 
-switch = 0
-prev_switch = 1
-prev_poss = data[1]['possession']
-poss_list = []
+  with open('3749052.json') as f:
+      data = json.load(f)
 
-# find data ranges of possession for each team
-for i in range(2,len(data)):
-  if data[i]['possession'] != data[i-1]['possession']:
-    switch += 1
-    if data[i]['possession_team']['name'] == 'Arsenal':
-      poss_list.append([prev_switch,i])
+  switch = 0
+  prev_switch = 1
+  prev_poss = data[1]['possession']
+  poss_list = []
 
-    prev_switch = i
+  # find data ranges of possession for each team
+  for i in range(2,len(data)):
+    #print(i,data[i]['possession_team']['name'],data[i]['possession'],prev_switch)
+    if data[i]['possession'] != data[i-1]['possession']:
+      switch += 1
+      if data[i-1]['possession_team']['name'] == 'Arsenal':
+        poss_list.append([prev_switch,i])
 
-print(data[101])
-print('switch',switch)
+      prev_switch = i
 
-for ii in range(4,7):
-  fig, ax=plt.subplots()
-  tl.get_pitch()
-  for i in range(poss_list[ii][0], poss_list[ii][1]):
-    print(ii,i,data[i]['type']['name'],data[i]['player']['name'],data[i]['timestamp'],data[i]['possession_team']['name'])
-    #print(data[i]) ; print('')
-    value = data[i]['type']['name']
+  print(data[101])
+  print('switch',switch)
 
-    if value == 'Pass':
-      print('pass')
-      X1 = data[i]['location'][0]
-      Y1 = data[i]['location'][1]
-      
-      X2 = data[i]['pass']['end_location'][0]
-      Y2 = data[i]['pass']['end_location'][1]
+  #for i in range(len(poss_list)):
+  #  print(poss_list[i])
 
-      if 'outcome' in data[i]['pass']:
-        plt.plot(X1,Y1,'o',color='gray')
-        plt.plot([X1,X2],[Y1,Y2],color='red')
-        plt.plot(X2,Y2,'X',color='red')
+  # create list of each player and event in each possession pathway
+  poss_data = []
+  poss_name_data = []
+  for i in range(2,len(poss_list)):
+    poss_data.append([])
+    poss_name_data.append([])
+    new_list = [] ; new_name_list = []
+
+    for j in range(poss_list[i][0], poss_list[i][1]):
+      try:
+        new_list.append([data[j]['player']['id'],data[j]['type']['id']])
+        new_name_list.append([data[j]['player']['name'],data[j]['type']['name']])
+      except KeyError:
+        pass
+
+    poss_data[i-2] = new_list
+    poss_name_data[i-2] = new_name_list
+
+  # determine pathway points for each possession
+  poss_score = len(poss_list)*[None]
+  for i in range(2,len(poss_list)):
+  #for i in range(2,10):
+    val = poss_list[i][1]-1
+
+    if 1==0:
+      try:
+        print(data[val]['type']['name'],data[val]['player']['name'],data[val]['play_pattern']['name'],data[val]['possession_team']['name'],data[val]['possession'])
+        print(data[val+1]['type']['name'],data[val+1]['player']['name'],data[val+1]['play_pattern']['name'],data[val+1]['possession_team']['name'],data[val+1]['possession'])
+        print('')
+        if data[val+1]['possession_team']['name'] != 'Arsenal':
+          print('whoops bad result')
+      except KeyError:
+        pass
+
+    try:
+
+      if data[val+1]['possession_team']['name'] == 'Arsenal':  # possesion stays
+        if data[val+1]['play_pattern']['id'] == 2:  # caused a corner kick
+          poss_score[i] = 3
+        elif data[val+1]['play_pattern']['id'] == 3:  # caused a free kick
+          poss_score[i] = 2
+        elif data[val+1]['play_pattern']['id'] == 4:  # caused a throw in
+          poss_score[i] = 1
+        elif data[val+1]['play_pattern']['id'] == 7:  # caused a goal kick
+          poss_score[i] = 1
+        else:
+          print('NEW TYPE')
+        
+      elif data[val+1]['possession_team']['name'] != 'Arsenal':  # possesion changes
+        if data[val+1]['play_pattern']['id'] == 2:  # caused a corner kick 
+          poss_score[i] = -3
+        elif data[val+1]['play_pattern']['id'] == 3:  # caused a free kick
+          poss_score[i] = -2
+        elif data[val+1]['play_pattern']['id'] == 4:  # caused a throw in
+          poss_score[i] = -1
+        elif data[val+1]['play_pattern']['id'] == 7:  # caused a goal kick
+          poss_score[i] = -1
+
+        elif data[val+1]['play_pattern']['id'] == 1:  # open play, gave ball away
+            poss_score[i] = -2
+
+        elif:
+          # this is not done yet, bed
+          print(data[val]['goalkeeper']['type']['name'])
+
+        else:
+          print('NEW NEW')
+          print(data[val+1]['play_pattern']['name'])
+          print(data[val]['type']['name'],data[val]['play_pattern']['name'])
+          print(data[val]['goalkeeper']['type']['name'])
+          #print(data[val-1])
+          print('')
+
       else:
-        plt.plot(X1,Y1,'o',color='gray')
-        plt.plot([X1,X2],[Y1,Y2],color='green')
-        plt.plot(X2,Y2,'o',color='green')
+        print('WTF')
+
+    except KeyError as e:
+      print('========================================================')
+      print(str(e))
+      print(data[val+1])
+      print('========================================================')
+
+  print(poss_score)
+
+
+  # plot the possession pathways
+  for ii in range(12,10):
+    fig, ax=plt.subplots()
+    pl.get_pitch()
+    pl.plot_pass_path(data,poss_list[ii][0],poss_list[ii][1])
     
-    elif value == 'Dribble' or value == 'Ball Receipt*':
-      print('dribble',' ',value)
-      X1 = data[i]['location'][0]
-      Y1 = data[i]['location'][1]
-      plt.plot(X1,Y1,'D',color='blue')
+  plt.show()
+#=========================================================================================================================#
 
-    elif value == 'Shot':
-      print('shot')
-      X1 = data[i]['location'][0]
-      Y1 = data[i]['location'][1]
-      plt.plot(X1,Y1,'*',color='yellow')
 
-    elif value == 'Pressure' or value == 'Duel' or value == 'Dribbled Past':
-      pass
-    
-    else:
-      print('other ',value)
-      X1 = data[i]['location'][0]
-      Y1 = data[i]['location'][1]
-      plt.plot(X1,Y1,'o',color='orange')
-
-    plt.title(data[i]['timestamp'])
-    plt.pause(1.0)
-
-plt.show()
+if __name__ == "__main__":
+    main()
---
+++ plots.py
@@ -0,0 +1,88 @@
+#===============================================================================================#
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+
+#===============================================================================================#
+def get_pitch():
+    #Pitch Outline & Centre Line
+    plt.plot([0,0],[0,90], color="black")
+    plt.plot([0,130],[90,90], color="black")
+    plt.plot([130,130],[90,0], color="black")
+    plt.plot([130,0],[0,0], color="black")
+    plt.plot([65,65],[0,90], color="black")
+
+    #Left Penalty Area
+    plt.plot([16.5,16.5],[65,25],color="black")
+    plt.plot([0,16.5],[65,65],color="black")
+    plt.plot([16.5,0],[25,25],color="black")
+
+    #Right Penalty Area
+    plt.plot([130,113.5],[65,65],color="black")
+    plt.plot([113.5,113.5],[65,25],color="black")
+    plt.plot([113.5,130],[25,25],color="black")
+
+    #Left 6-yard Box
+    plt.plot([0,5.5],[54,54],color="black")
+    plt.plot([5.5,5.5],[54,36],color="black")
+    plt.plot([5.5,0.5],[36,36],color="black")
+
+    #Right 6-yard Box
+    plt.plot([130,124.5],[54,54],color="black")
+    plt.plot([124.5,124.5],[54,36],color="black")
+    plt.plot([124.5,130],[36,36],color="black")
+#===============================================================================================#
+def plot_pass_path(data,start_val,end_val):
+    # creates a movie of individual events during a possession sequence
+
+    for i in range(start_val, end_val):
+
+        value = data[i]['type']['name']
+        val_id = data[i]['type']['id']
+        type_val = 1
+
+        if data[i]['team']['name'] != 'Arsenal':
+            value = 'Pressure'
+            val_id = 17
+
+        if val_id == 30:  # pass
+            X1 = data[i]['location'][0]
+            Y1 = data[i]['location'][1]
+        
+            X2 = data[i]['pass']['end_location'][0]
+            Y2 = data[i]['pass']['end_location'][1]
+
+            if 'outcome' in data[i]['pass']:
+                plt.plot(X1,Y1,'o',color='gray')
+                plt.plot([X1,X2],[Y1,Y2],color='red')
+                plt.plot(X2,Y2,'X',color='red')
+            else:
+                plt.plot(X1,Y1,'o',color='gray')
+                plt.plot([X1,X2],[Y1,Y2],color='green')
+                plt.plot(X2,Y2,'o',color='green')
+        
+        elif val_id in [14, 42]:  # dribble or ball receipt
+            X1 = data[i]['location'][0]
+            Y1 = data[i]['location'][1]
+            plt.plot(X1,Y1,'D',color='blue')
+
+        elif val_id == 16:      # shot
+            X1 = data[i]['location'][0]
+            Y1 = data[i]['location'][1]
+            plt.plot(X1,Y1,'*',color='yellow')
+
+        elif val_id in [4, 17, 22, 39]:   # duel, pressure, foul committed, dribbled past
+            type_val = 0
+        
+        else:
+            X1 = data[i]['location'][0]
+            Y1 = data[i]['location'][1]
+            plt.plot(X1,Y1,'o',color='gray')
+
+        if type_val == 1:
+            plt.title(data[i]['timestamp']+'\n '+value+' '+data[i]['player']['name'])
+            plt.pause(1.20)
+
+    #if i == poss_list[ii][1]-1:
+    #  print('Next action ', data[i+1]['index'] ,data[i+1]['type']['name'], data[i+1]['player']['name'], data[i+1]['play_pattern']['name'])
+#===============================================================================================#
