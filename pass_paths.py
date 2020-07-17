5472558bbdfa3efcf040d45a23763f6c3cd3505e
Rob Wells <robert.wells@colorado.edu>
2020-07-16 22:13:01 -0600

    indiv scores not finished

--- pass_paths.py
+++ pass_paths.py
@@ -12,6 +12,9 @@ import plots as pl
 #=========================================================================================================================#
 
 def main():
+  # data contains json information 
+  # poss_list contains start and end data endices for each possesion  (focusing on Arsenal) 7/16
+  # poss_data/poss_name_data gives the id/name of each player and event type for paths 
 
   with open('3749052.json') as f:
       data = json.load(f)
@@ -20,22 +23,31 @@ def main():
   prev_switch = 1
   prev_poss = data[1]['possession']
   poss_list = []
+  player_list = []
 
   # find data ranges of possession for each team
-  for i in range(2,len(data)):
-    #print(i,data[i]['possession_team']['name'],data[i]['possession'],prev_switch)
-    if data[i]['possession'] != data[i-1]['possession']:
+  # create list of all players in the game
+  for i in range(len(data)):
+    if data[i]['type']['id'] in [19, 35]: # Starting XI and subs
+      if data[i]['type']['id'] == 35:
+        if data[i]['team']['name'] == 'Arsenal':
+          for i in data[i]['tactics'].get('lineup'):
+            player_list.append([i['player']['id'],i['player']['name']])
+      elif data[i]['type']['id'] == 19:
+        if data[i]['team']['name'] == 'Arsenal':
+          player_list.append([data[i]['substitution']['replacement']['id'], 
+          data[i]['substitution']['replacement']['name']])
+    
+    elif data[i]['possession'] != data[i-1]['possession']:
       switch += 1
       if data[i-1]['possession_team']['name'] == 'Arsenal':
         poss_list.append([prev_switch,i])
 
       prev_switch = i
 
-  print(data[101])
+  #print(data[101])
   print('switch',switch)
 
-  #for i in range(len(poss_list)):
-  #  print(poss_list[i])
 
   # create list of each player and event in each possession pathway
   poss_data = []
@@ -55,24 +67,38 @@ def main():
     poss_data[i-2] = new_list
     poss_name_data[i-2] = new_name_list
 
-
   # determine pathway points for each possession
   poss_score = tl.get_path_score(data,poss_list)
-  #print(poss_score)
-  #print(poss_list)
+
+  # find player score (score of all the pathways they are in)
+  total_data = [poss_score,poss_data]
+
+  indiv_score = []
+  for id_val, type_val in player_list:
+    score = 0
+    for i in range(len(poss_data)):
+      if id_val in poss_data[i][0][0]:
+        score += poss_score
+
+    indiv_score.append(score)
+
+  print(indiv_score)  
+    # would like to just add the score to the player_list
+    # player_list[].insert()
 
   ## use techniques of machine learning to find the features that make the highest scoring possession pathways ##
   # sort by length, number of players, etc.
 
-  # find player score (score of all the pathways they are in)
-
+  #print(poss_data)
+  print(np.shape(poss_data))
+  print(len(poss_data[0]))
 
   # plot the possession pathways 
   sort_poss_list = [x for _,x in sorted(zip(poss_score,poss_list), reverse=True)]
   sort_poss_score = sorted(poss_score, reverse=True)
 
 
-  for ii in range(0,3):
+  for ii in range(4,3):
     fig, ax=plt.subplots()
     pl.get_pitch()
     pl.plot_pass_path(data,sort_poss_list[ii][0],sort_poss_list[ii][1])
