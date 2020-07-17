d144520856670fd437b00930ce415e5008e783e3
Rob Wells <vaandei421@gmail.com>
2020-07-16 19:24:47 -0600

    pathway scoring

--- pass_paths.py
+++ pass_paths.py
@@ -55,78 +55,27 @@ def main():
     poss_data[i-2] = new_list
     poss_name_data[i-2] = new_name_list
 
+
   # determine pathway points for each possession
-  poss_score = len(poss_list)*[None]
-  for i in range(2,len(poss_list)):
-  #for i in range(2,10):
-    val = poss_list[i][1]-1
+  poss_score = tl.get_path_score(data,poss_list)
+  #print(poss_score)
+  #print(poss_list)
 
-    if 1==0:
-      try:
-        print(data[val]['type']['name'],data[val]['player']['name'],data[val]['play_pattern']['name'],data[val]['possession_team']['name'],data[val]['possession'])
-        print(data[val+1]['type']['name'],data[val+1]['player']['name'],data[val+1]['play_pattern']['name'],data[val+1]['possession_team']['name'],data[val+1]['possession'])
-        print('')
-        if data[val+1]['possession_team']['name'] != 'Arsenal':
-          print('whoops bad result')
-      except KeyError:
-        pass
+  ## use techniques of machine learning to find the features that make the highest scoring possession pathways ##
+  # sort by length, number of players, etc.
 
-    try:
-
-      if data[val+1]['possession_team']['name'] == 'Arsenal':  # possesion stays
-        if data[val+1]['play_pattern']['id'] == 2:  # caused a corner kick
-          poss_score[i] = 3
-        elif data[val+1]['play_pattern']['id'] == 3:  # caused a free kick
-          poss_score[i] = 2
-        elif data[val+1]['play_pattern']['id'] == 4:  # caused a throw in
-          poss_score[i] = 1
-        elif data[val+1]['play_pattern']['id'] == 7:  # caused a goal kick
-          poss_score[i] = 1
-        else:
-          print('NEW TYPE')
-        
-      elif data[val+1]['possession_team']['name'] != 'Arsenal':  # possesion changes
-        if data[val+1]['play_pattern']['id'] == 2:  # caused a corner kick 
-          poss_score[i] = -3
-        elif data[val+1]['play_pattern']['id'] == 3:  # caused a free kick
-          poss_score[i] = -2
-        elif data[val+1]['play_pattern']['id'] == 4:  # caused a throw in
-          poss_score[i] = -1
-        elif data[val+1]['play_pattern']['id'] == 7:  # caused a goal kick
-          poss_score[i] = -1
-
-        elif data[val+1]['play_pattern']['id'] == 1:  # open play, gave ball away
-            poss_score[i] = -2
-
-        elif:
-          # this is not done yet, bed
-          print(data[val]['goalkeeper']['type']['name'])
-
-        else:
-          print('NEW NEW')
-          print(data[val+1]['play_pattern']['name'])
-          print(data[val]['type']['name'],data[val]['play_pattern']['name'])
-          print(data[val]['goalkeeper']['type']['name'])
-          #print(data[val-1])
-          print('')
-
-      else:
-        print('WTF')
-
-    except KeyError as e:
-      print('========================================================')
-      print(str(e))
-      print(data[val+1])
-      print('========================================================')
-
-  print(poss_score)
+  # find player score (score of all the pathways they are in)
 
 
-  # plot the possession pathways
-  for ii in range(12,10):
+  # plot the possession pathways 
+  sort_poss_list = [x for _,x in sorted(zip(poss_score,poss_list), reverse=True)]
+  sort_poss_score = sorted(poss_score, reverse=True)
+
+
+  for ii in range(0,3):
     fig, ax=plt.subplots()
     pl.get_pitch()
-    pl.plot_pass_path(data,poss_list[ii][0],poss_list[ii][1])
+    pl.plot_pass_path(data,sort_poss_list[ii][0],sort_poss_list[ii][1])
     
   plt.show()
 #=========================================================================================================================#
--- plots.py
+++ plots.py
@@ -2,7 +2,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import json
-
 #===============================================================================================#
 def get_pitch():
     #Pitch Outline & Centre Line
